9a975bd79e6db227de0f02ba6ba98f3d3c682bf0
cx-Zh <491907188@qq.com>
2020-06-24 22:46:20 +0800

    finish p2s2

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,51 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = {"levelNumber=2"})
 class DemoSpringConfigApplicationTests {
+
+	@Autowired
+	private LevelController levelController;
 
 	@Test
 	void contextLoads() {
+		String result = levelController.getLevel();
+		String expect = "basic";
+		assertThat(result.equals(expect));
+	}
+
+}
+
+@SpringBootTest(properties = {"levelNumber=1"})
+class extend1 extends DemoSpringConfigApplicationTests {
+
+	@Autowired
+	private LevelController levelController;
+
+	@Test
+	void contextLoads() {
+		String result = levelController.getLevel();
+		String expect = "basic";
+		assertThat(result.equals(expect));
+	}
+
+}
+@SpringBootTest(properties = {"levelNumber=0"})
+class extend2 extends DemoSpringConfigApplicationTests {
+
+	@Autowired
+	private LevelController levelController;
+
+	@Test
+	void contextLoads() {
+		String result = levelController.getLevel();
+		String expect = "advanced";
+		assertThat(result.equals(expect));
 	}
 
 }
